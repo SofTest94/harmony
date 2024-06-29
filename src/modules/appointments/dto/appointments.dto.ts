758357b7bdaf6c78f450488c21c998785daac901
SofTest <softest94@gmail.com>
2024-06-29 00:25:51 -0600

    appointments

--- src/modules/appointments/dto/appointments.dto.ts
+++ src/modules/appointments/dto/appointments.dto.ts
@@ -5,7 +5,7 @@ export class AppointmentsDTO {
   idService?: ObjectId; // 662b108dd25ce70194ea5974
   date: string;         // 1 de julio
   hour:string;          // 4:30 pm
-  status: string;       // pagado - pendiente de pago - cancelado - reagendado
+  status: string;       // pagado - pendiente de pago - cancelado - reagendado - cerrado
   }
   
 
